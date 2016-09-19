28a92855c61b5edd30926cc72c7fc88f211b576f
Opiumtm <opium_tm@live.ru>
2016-09-19 17:46:01 +0300

    RSS xml examples + namespace consts

---
+++ Ipatov.PodcastRssClient/Ipatov.PodcastRssClient/Podcast/Internal/Consts.cs
@@ -0,0 +1,18 @@
+﻿namespace Ipatov.PodcastRssClient.Podcast.Internal
+{
+    /// <summary>
+    /// Constants.
+    /// </summary>
+    public static class Consts
+    {
+        /// <summary>
+        /// itunes namespace.
+        /// </summary>
+        public const string ItunesNamespace = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+
+        /// <summary>
+        /// atom namespace.
+        /// </summary>
+        public const string AtomNamespace = "http://www.w3.org/2005/Atom";
+    }
+}
